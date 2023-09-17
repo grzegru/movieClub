87b97b0bd1c60ed95d3a945efc34a4f20588c6d2
grzegru <grzegorz.a.gruszczynski@student.put.poznan.pl>
2023-09-18 00:10:11 +0200

    first commit, home page + movie page

---
+++ src/main/resources/db/testdata/0001_genres.sql
@@ -0,0 +1,10 @@
+insert into genre (name) values ('Drama');
+insert into genre (name) values ('Animation');
+insert into genre (name) values ('Documentary|Sci-Fi');
+insert into genre (name) values ('Comedy|Fantasy|Horror');
+insert into genre (name) values ('Thriller');
+insert into genre (name) values ('Drama|Romance');
+insert into genre (name) values ('Action|Adventure|Comedy');
+insert into genre (name) values ('History');
+insert into genre (name) values ('Romance');
+insert into genre (name) values ('Children|Comedy|Fantasy');
